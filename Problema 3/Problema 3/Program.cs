df7e01d43b85cedccedb5a73d1708d4949d0d68f
mrstevengz <ssequeira@uamv.edu.ni>
2024-10-17 17:10:13 -0600

    Primer Commit Ejercicio 3

---
+++ Problema 3/Problema 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Traductor
+{
+    static void Main()
+    {
+        List<(string, string)> diccionario = CrearDiccionario();
+        Traducir(diccionario);
+    }
+
+    static List<(string, string)> CrearDiccionario()
+    {
+        List<(string, string)> diccionario = new List<(string, string)>();
+        Console.WriteLine("Introduce las parejas de palabras (Ingles/Español): ");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Ingles: ");
+            string ingles = Console.ReadLine();
+            Console.Write("Español: ");
+            string espanol = Console.ReadLine();
+            diccionario.Add((ingles, espanol));
+        }
+        return diccionario;
+    }
+
+}
