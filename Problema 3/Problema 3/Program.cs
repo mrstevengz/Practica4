683163dc46f4851c72d3896dd06b71b7270ca1e2
mrstevengz <ssequeira@uamv.edu.ni>
2024-10-18 08:19:29 -0600

    Ultimo Commit Ejercicio 3

--- Problema 3/Problema 3/Program.cs
+++ Problema 3/Problema 3/Program.cs
@@ -4,56 +4,45 @@ class Traductor
 {
     static void Main()
     {
-        List<(string, string)> diccionario = CrearDiccionario();
+        List<Tuple<string, string>> diccionario = CrearDiccionario();
         Traducir(diccionario);
+        Console.ReadKey();
     }
 
-    static List<(string, string)> CrearDiccionario()
+    public static List<Tuple<string, string>> CrearDiccionario()
     {
-        List<(string, string)> diccionario = new List<(string, string)>();//Inicializacion de la lista para almacenar las parejas de palabras
+        List<Tuple<string, string>> diccionario = new List<Tuple<string, string>>();//Inicializacion de la lista para almacenar las parejas de palabras
         Console.WriteLine("Introduce las parejas de palabras (Ingles/Español): ");
 
         for (int i = 0; i < 5; i++)
         {
             Console.Write("Ingles: ");
-            string ingles = Console.ReadLine();
+            string palabra1 = Console.ReadLine();
             Console.Write("Español: ");
-            string espanol = Console.ReadLine();
-            diccionario.Add((ingles, espanol));
+            string palabra2 = Console.ReadLine();
+            diccionario.Add(new Tuple<string, string>(palabra1, palabra2));
         }
         return diccionario;
     }
 
-    static void Traducir(List<(string, string)> diccionario)
+    static void Traducir(List<Tuple<string, string>> diccionario)
     {
-        while (true)
-        {
-            Console.WriteLine("Bienvenido al modo de traduccion. \nIntroduce una palabra en ingles para traducir (o escribe salir para terminar): ");
-            string palabra = Console.ReadLine();
+        Console.WriteLine("Introduce la palabra a traducir: ");
+        string palcomp = Console.ReadLine();
+        bool encontrado = false;
 
-            if (palabra == "salir") //Verificacion para permitir al usuario salir del programa
+        foreach (var duo in diccionario)
+        {
+            if (duo.Item1.Equals(palcomp, StringComparison.OrdinalIgnoreCase))
             {
+                Console.WriteLine("Traduccion: " + duo.Item2);
+                encontrado = true;
                 break;
             }
-
-            string traduccion = null;
-            foreach (var par in diccionario) //Buscar la traduccion en el diccionario
-            {
-                if (par.Item1 == palabra)
-                {
-                    traduccion = par.Item2;
-                    break;
-                }
-            }
-
-            if (traduccion != null) //Verificacion para mostrar la traduccion si se encuentra, o mandar un error
-            {
-                Console.WriteLine($"La traducción de '{palabra}' es '{traduccion}'.");
-            }
-            else
-            {
-                Console.WriteLine("La palabra no se encuentra en el diccionario.");
-            }
         }
+
+        if (!encontrado)
+            Console.WriteLine("No se encontro la palabra en el diccionario.");
+
     }
 }
